a0c199d62c326f5daeda2ba374f1335faf41cbd2
congconhavesinh <congconhavesinh272@gmail.com>
2024-06-25 16:30:56 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,25 +1,27 @@
 function upDate(previewPic) {
-    // Use console.log to check the event is triggering
     console.log("Mouseover event triggered");
     console.log("Alt text:", previewPic.alt);
     console.log("Source:", previewPic.src);
 
-    // Update the text of the element with id 'image'
     const imageDiv = document.getElementById('image');
     imageDiv.innerHTML = previewPic.alt;
-
-    // Update the background image of the element with id 'image'
     imageDiv.style.backgroundImage = `url(${previewPic.src})`;
 }
 
 function unDo() {
-    // Reset the background image and text
     const imageDiv = document.getElementById('image');
     imageDiv.style.backgroundImage = 'url()';
     imageDiv.innerHTML = 'Hover over an image below to display here.';
 }
 
-// Adding event listeners to each preview image
+function addTabFocus() {
+    console.log("Page loaded, adding tabindex attributes.");
+    const previews = document.querySelectorAll('.preview');
+    previews.forEach((preview, index) => {
+        preview.setAttribute('tabindex', index + 1); // tabindex should start from 1
+    });
+}
+
 const previews = document.querySelectorAll('.preview');
 previews.forEach(preview => {
     preview.addEventListener('mouseover', function() {
